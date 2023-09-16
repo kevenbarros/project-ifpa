74f3c2a83f2a49d0d481c2dca47e3b406d816041
kevenbarros <kevenwilliamsilvabarros@gmail.com>
2023-09-16 10:34:30 -0300

    teste

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 // import './App.css'
-import {Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import Loading from '../loading'
 import Mic from '../../assets/mic.svg'
 import Home from '../../assets/home.svg'
@@ -10,51 +10,50 @@ import Moon from '../../assets/logo.svg'
 import MoonBp from '../../assets/logopb.svg'
 import PerfilMock from '../../assets/perfilMock.png'
 
-interface IOptionsSideBar{
+interface IOptionsSideBar {
   label: string,
   icone: any
 }
 function SideBar() {
-    const options:IOptionsSideBar[] = [
-      {
-        label: 'home', icone: MoonBp
-      },
-      {
-        label: 'audio', icone: Mic
-      },
-      {
-        label: 'home', icone: Home
-      },
-      {
-        label: 'metas', icone: Rockt
-      },
-      {
-        label: 'configuracao', icone: Settings
-      },
-      {
-        label: 'configuracao', icone: PerfilMock
-      },
-      // {
-      //   label: 'Pie', icone: Pie
-      // }
-    ]
-    return (
-      <>
-          <div style={{ backgroundColor: "#008F98", display: "flex", justifyContent: "space-around"}}>
-            <div style={{width: "60%", display: "flex", alignItems: 'center', justifyContent: 'space-evenly', padding: "10px 0"}}>
-              {
-                options.map((e)=>{
-                  return <Link to={`/app/${e.label}`}><img src={e.icone} style={{cursor: "pointer", maxWidth: 40}} alt="" /></Link>
+  const options: IOptionsSideBar[] = [
+    {
+      label: 'home', icone: MoonBp
+    },
+    {
+      label: 'audio', icone: Mic
+    },
+    {
+      label: 'home', icone: Home
+    },
+    {
+      label: 'metas', icone: Rockt
+    },
+    {
+      label: 'configuracao', icone: Settings
+    },
+    {
+      label: 'configuracao', icone: PerfilMock
+    },
+    // {
+    //   label: 'Pie', icone: Pie
+    // }
+  ]
+  return (
+    <>
+      <div style={{ backgroundColor: "#008F98", display: "flex", justifyContent: "space-around" }}>
+        <div style={{ width: "60%", display: "flex", alignItems: 'center', justifyContent: 'space-evenly', padding: "10px 0" }}>
+          {
+            options.map((e) => {
+              return <Link to={`/app/${e.label}`}><img src={e.icone} style={{ cursor: "pointer", maxWidth: 40 }} alt="teste" /></Link>
 
-                })
-              }
-            </div>
-              
-          </div>
-              <Outlet />
-      </>
-    )
-  }
-  
-  export default SideBar
-  
+            })
+          }
+        </div>
+
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+export default SideBar
